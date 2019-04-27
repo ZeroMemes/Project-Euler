87bd3f58d9ec73841f1cf458a2632a88627bbfd7
Brady <zeromemesdev@gmail.com>
2019-04-27 18:25:41 -0500

    Euler024

---
+++ Euler024/Euler024.go
@@ -0,0 +1,32 @@
+package main
+
+import "sort"
+
+func main() {
+	permutations := getPermutations("0123456789")
+	sort.Strings(permutations)
+	println(permutations[999999])
+}
+
+func getPermutations(str string) []string {
+	permutations := make([]string, 0)
+	permutate(&permutations, str, 0)
+	return permutations
+}
+
+func permutate(permutations *[]string, str string, index int) {
+	if index == len(str)-1 {
+		*permutations = append(*permutations, str)
+		return
+	}
+
+	for i := index; i < len(str); i++ {
+		// Swap characters
+		chars := []rune(str)
+		ci := chars[i]
+		chars[i] = chars[index]
+		chars[index] = ci
+
+		permutate(permutations, string(chars), index+1)
+	}
+}
