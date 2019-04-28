9655588694fa5b8eae1cd8517b2bc6ac65e0ecae
Brady <zeromemesdev@gmail.com>
2019-04-27 23:18:42 -0500

    Go is so epic

--- Euler025/Euler025.go
+++ Euler025/Euler025.go
@@ -7,9 +7,7 @@ func main() {
 	n2 := big.NewInt(1)
 	index := 2
 	for len(n2.String()) < 1000 {
-		temp := n2
-		n2 = n1.Add(n1, n2)
-		n1 = temp
+		n1, n2 = n2, n1.Add(n1, n2)
 		index++
 	}
 	println(index)
